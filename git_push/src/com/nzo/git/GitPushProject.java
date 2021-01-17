f8e4b965bd4ae8e511fe1e40641cc0f70c5854a8
Nzo <526209848@qq.com>
2021-01-17 15:54:37 +0800

    提交

---
+++ git_push/src/com/nzo/git/GitPushProject.java
@@ -0,0 +1,13 @@
+package com.nzo.git;
+
+public class GitPushProject {
+    public static void main(String[] args) {
+        int i = 3;
+        int j =4;
+        System.out.println(add(i, j));
+    }
+
+    private static int add(int i, int j) {
+        return i+j;
+    }
+}
